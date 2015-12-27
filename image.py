2ec854e391872e15b1352bc1c2f2b5d04e8cdb9c
Nico <nicolasbrailo@gmail.com>
2015-12-27 13:15:23 +0100

    Reading date

--- image.py
+++ image.py
@@ -1,8 +1,12 @@
+import pyexiv2
 
 class Image(object):
     def __init__(self, fullpath, fname):
         self.fullpath = fullpath
         self.fname = fname
+
+        self._metadata = pyexiv2.ImageMetadata(self.fullpath)
+        self._metadata.read()
 
     def get_fullpath(self):
         return self.fullpath
@@ -11,7 +15,13 @@ class Image(object):
         return self.fname
 
     def get_date(self):
-        return "0/0/0"
+        key = "Exif.Photo.DateTimeOriginal"
+        try:
+            return self._metadata[key].value.strftime('%d %B %Y, %H:%M:%S')
+        except KeyError:
+            return ""
+        except Exception, e:
+            print ("can not get date/time '%s' for image '%s': %s" % (self._metadata[key].raw_value, self.path_full, str(e)))
 
     def get_position(self):
         return "41.1234 / 12.1234"
