083a68009788768b7d227563e720abdfa0d0327e
Nico <nicolasbrailo@gmail.com>
2015-12-27 13:25:58 +0100

    Reading gps pos

--- image.py
+++ image.py
@@ -24,5 +24,26 @@ class Image(object):
             print ("can not get date/time '%s' for image '%s': %s" % (self._metadata[key].raw_value, self.path_full, str(e)))
 
     def get_position(self):
-        return "41.1234 / 12.1234"
+        lat_key_ref = 'Exif.GPSInfo.GPSLatitudeRef' # S/N
+        lat_key = 'Exif.GPSInfo.GPSLatitude'
+        lat = str(self._sex_to_dec(self._metadata[lat_key].value)) + self._metadata[lat_key_ref].raw_value
 
+        lon_key_ref = 'Exif.GPSInfo.GPSLongitudeRef' # West/East
+        lon_key = 'Exif.GPSInfo.GPSLongitude'
+        lon = str(self._sex_to_dec(self._metadata[lon_key].value)) + self._metadata[lon_key_ref].raw_value
+
+        return lat + ' ' + lon
+
+    def _dec_to_sex(self, x):
+        degrees = int(math.floor(x))
+        minutes = int(math.floor(60 * (x - degrees)))
+        seconds = int(math.floor(6000 * (60 * (x - degrees) - minutes)))
+        return (pyexiv2.utils.make_fraction(degrees, 1), pyexiv2.utils.make_fraction(minutes, 1), pyexiv2.utils.make_fraction(seconds, 100))
+
+    def _sex_to_dec(self, fractions):
+        degrees = float(fractions[0])
+        minutes = float(fractions[1])
+        seconds = float(fractions[2])    
+        minutes = minutes + (seconds/60)
+        degrees = degrees + (minutes/60)
+        return degrees
